bbf2e4244d8fefaf85393d7a39dba7c99dd5f8c2
tomi-0 <asadetomi@gmail.com>
2025-10-27 20:12:11 +0000

    delete task button working

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 // Create array of objects for tasks
-const tasks = []
+let tasks = []
 const completedtasks = []
 
-// Collect task description from input when button clciken
-const addTaskButtonElement = document.querySelector(".js-add-button");
-const inputField = document.querySelector(".js-task-desc");
-
-addTaskButtonElement.addEventListener("click", () => {
-
-    if (inputField.value && inputField.value[0]!==" ") {
-        const newTask = {
-            // id either 0 or previous id incremented
-            id: (tasks[tasks.length-1]) ? tasks[tasks.length-1].id + 1 : 0,
-            desciption: inputField.value
-        }
-        tasks.push(newTask);
-        // add task to display
-        renderTasks();
-        console.log(tasks);
-        console.log(document.querySelectorAll(".js-delete-button"));
-    }
-    
-    // clear input field
-    inputField.value = '';
-});
+renderTasks();
 
 // render task list
 function renderTasks() {
@@ -35,23 +14,56 @@ function renderTasks() {
          <div class="task">
                 <div class="task-desc">${task.desciption}</div>
                 <button class="completed-button"></button>
-                <button class="delete-button js-delete-button data-task-id=${task.id}"></button>
+                <button class="delete-button js-delete-button" data-task-id=${task.id}></button>
         </div>
     `;
     })
     tasksElement.innerHTML = generatedHTML;
+
+    // Collect task description from input when button clciken
+    const addTaskButtonElement = document.querySelector(".js-add-button");
+    const inputField = document.querySelector(".js-task-desc");
+
+    addTaskButtonElement.addEventListener("click", () => {
+
+        if (inputField.value && inputField.value[0]!==" ") {
+            const newTask = {
+                // id either 0 or previous id incremented
+                id: parseInt((tasks[tasks.length-1]) ? tasks[tasks.length-1].id + 1 : 0),
+                desciption: inputField.value
+            }
+            tasks.push(newTask);
+            // add task to display
+            renderTasks();
+            console.log(tasks);
+            console.log(document.querySelectorAll(".js-delete-button"));
+        }
+        
+        // clear input field
+        inputField.value = '';
+    });
+
+
+    // deletes an existing task
+    document.querySelectorAll(".js-delete-button").forEach((deleteButton) => {
+
+        deleteButton.addEventListener("click", () => {
+            const id = parseInt(deleteButton.dataset.taskId);
+
+            tasks = tasks.filter((task) => {
+                if (task.id === id ) {
+                    return false 
+                } return true;
+                //console.log("taskId:",task.id);
+                //console.log("id:",id);
+            });
+            
+            console.log(tasks);
+            renderTasks();
+        });
+    });
 }
 
-// deletes an existing task
-document.querySelectorAll(".js-delete-button").forEach((deleteButton) => {
-    deleteButton.addEventListener('click', () => {
-        const id = deleteButton.dataset.taskId;
-        console.log(id);
-        tasks = tasks.filter((task) => task.id !== id);
-        console.log(tasks);
-    })
-    renderTasks();
-})
 
 
 
